bd35823374792fad9e274467d2e3a92284c82062
Victor Aketch <vickeryace@gmail.com>
2023-03-20 16:30:13 +0300

    Refactoring my controller.rb code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pizzas, only: [:index, :show]
   resources :restaurants, only: [:index, :show, :destroy]
-  # resources :restaurant_pizzas
+  resources :restaurant_pizzas, only: [:create, :index]
  
 end
