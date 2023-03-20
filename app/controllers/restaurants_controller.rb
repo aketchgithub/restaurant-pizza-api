bb9aa60d0e4a60fcb628c4f6f816e6cbd0f0fb87
Victor Aketch <vickeryace@gmail.com>
2023-03-20 16:10:28 +0300

    Completed the first two deliverables

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,41 @@
 class RestaurantsController < ApplicationController
+    wrap_parameters format: []
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index 
         restaurants = Restaurant.all 
         render json: restaurants.as_json(only: [:id, :name, :address])
     end
 
+    def show
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant.nil?
+          render json: { error: "Restaurant not found" }, status: :not_found
+        else
+          render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } }, only: [:id, :name, :address])
+        end
+    end
+
+    def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+    
+        if restaurant
+          restaurant.restaurant_pizzas.destroy_all
+          restaurant.destroy
+          head :no_content
+        else
+          render json: { error: "Restaurant not found" }, status: :not_found
+        end
+      end
+    # delete /restaurants/:id
+
     # private
 
-    # def 
+    # def find_restaurant
+    #     Restaurant.find_by(id: params[:id])
+    # end
+
+    # def render_not_found_response
+    #     render json: { error: "Restaurant not found"}, status: :not_found
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pizzas, only: [:index, :show]
-  resources :restaurants, only: [:index, :create]
+  resources :restaurants, only: [:index, :show, :destroy]
   # resources :restaurant_pizzas
  
 end
