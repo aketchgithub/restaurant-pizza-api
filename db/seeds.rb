529bec4e3a6680a4e83a1b6d975b13ee13298ad4
aketchgithub <vickeryace@gmail.com>
2023-04-04 09:43:57 +0300

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,30 @@
 require 'faker'
 
-# Create 5 restaurant records
 puts "📃 Seeding data..."
 
+# Create 5 restaurant records
 5.times do
-    Restaurant.create!(
-      name: Faker::Restaurant.name,
-      address: Faker::Address.street_address
-    )
-  end
-  
-  # Create 10 pizza records and associate each with a random restaurant record
-#   10.times do
-#     restaurant = Restaurant.all.sample
-#     pizza = Pizza.create!(
-#       name: Faker::Food.dish,
-#       ingredients: Faker::Food.ingredient,
-#       price: Faker::Number.decimal(l_digits: 2)
-#     )
-#     restaurant.pizzas << pizza
-#   end
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address
+  )
+end
 
-
+# Create 10 pizza records
 10.times do
-    restaurant_pizza = RestaurantPizza.create!(
-      restaurant: Restaurant.order("RANDOM()").first,
-      pizza: Pizza.order("RANDOM()").first,
-      price: rand(1..30)
-    )
-    puts "Created restaurant_pizza: #{restaurant_pizza.inspect}"
-  end
+  Pizza.create!(
+    name: Faker::Food.dish
+  )
+end
+
+# Create 10 restaurant_pizza records
+10.times do
+  restaurant_pizza = RestaurantPizza.create!(
+    restaurant: Restaurant.order("RANDOM()").first,
+    pizza: Pizza.order("RANDOM()").first,
+    price: rand(1..30)
+  )
+  puts "Created restaurant_pizza: #{restaurant_pizza.inspect}"
+end
 
 puts "✅ Done seeding"
